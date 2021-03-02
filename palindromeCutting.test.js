b6fd6243a9de16594566d734d3a66a344e992ab9
khanr <khanriza@gmail.com>
2021-03-01 21:42:36 -0500

    added palindromeCutting test

---
+++ palindromeCutting.test.js
@@ -0,0 +1,28 @@
+function palindromeCutting(s) {
+  if (!s.length) {
+    return "";
+  }
+
+  for (let x = s.split("").length; x > 0; x--) {
+    const word = s.split("").splice(0, x);
+    const prefixJoined = word.join("");
+    const prefixReversed = word.reverse().join("");
+    if (prefixJoined === prefixReversed) {
+      if (prefixJoined.length > 2) {
+        if (!s.replace(prefixJoined, "").length) {
+          return "";
+        }
+
+        return palindromeCutting(s.replace(prefixJoined, ""));
+      }
+    }
+  }
+}
+
+describe("palindromeCutting", () => {
+  it("should return nothing", () => {
+    const string = "aaacodedoc";
+    const results = palindromeCutting(string);
+    expect(results).toBe("");
+  });
+});
