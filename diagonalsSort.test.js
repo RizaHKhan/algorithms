e3138f25c7fdaf3c5d29232111ee9eed759eabdb
khanr <khanriza@gmail.com>
2021-03-04 22:53:09 -0500

    Additional changes

--- diagonalsSort.test.js
+++ diagonalsSort.test.js
@@ -1,9 +1,37 @@
 function diagonalSort(m) {
-  const rows = m.length; // 4
-  const cols = m[0].length; // 4
-  const diagonals = cols + rows - 1;
+  const rows = m.length;
+  const cols = m[0].length;
+  const diagonals = rows * cols - 1;
+  const flat = [];
 
-  for (let k = 0; x < diagonals; k++) {}
+  let newMatrix = Array(rows)
+    .fill(0)
+    .map((x, i) => Array(cols).fill("xxx"));
+
+  for (let k = 0; k <= diagonals; k++) {
+    const temp = [];
+    for (let y = cols - 1; y >= 0; y--) {
+      let x = k - y;
+
+      if (x >= 0 && x < rows) {
+        temp.push(m[y][x]);
+      }
+    }
+
+    if (temp.length) {
+      flat.push(temp.sort());
+    }
+  }
+
+  flat.forEach((valOutter, outterIndex) => {
+    let counter = outterIndex;
+    valOutter.forEach((valInner, innerIndex) => {
+      console.log(valInner, counter);
+      counter++;
+    });
+  });
+
+  console.log(newMatrix);
 }
 
 const matrix = [
@@ -15,74 +43,11 @@ const matrix = [
 
 diagonalSort(matrix);
 
-// describe("diagonalSort", () => {
-//   it("should return matrix sorted diagonally", () => {
-//     const a = [
-//       [1, 3, 9, 4],
-//       [9, 5, 7, 7],
-//       [3, 6, 9, 7],
-//       [1, 2, 2, 2],
-//     ];
-//     const results = diagonalSort(a);
-//     expect(results).toBe([
-//       [1, 9, 9, 7],
-//       [3, 5, 6, 9],
-//       [3, 4, 7, 7],
-//       [1, 2, 2, 2],
-//     ]);
-//   });
-// });
-
 /*
-
-[
-[0,0], [0,1], [0,2], [0, 3]
-[1,0], [1,1], [1,2], [1, 3],
-[2,0], [2,1], [2,2], [2, 3],
-[3,0], [3,1], [3,2], [3, 3],
-]
-
-const range = (lo, hi) => [...Array(hi - lo + 1)].map((_, i) => i + lo);
-
-const diagonals = (rows, cols) =>
-  range(0, rows + cols)
-    .map((d) =>
-      range(0, d)
-        .map((x) => [d - x, x])
-        .filter(([y, x]) => x >= 0 && x < cols && y >= 0 && y < rows)
-    )
-    .filter((xs) => xs.length > 0);
-
-const coordLens = (coords) => ({
-  get: (m) => coords.map(([x, y]) => m[x][y]),
-  set: (arr, m) =>
-    coords.reduce(
-      (m, [x, y], i) => {
-        m[x][y] = arr[i];
-        return m;
-      },
-      m.map((r) => [...r]) // cheap clone
-    ),
-});
-
-const diagonalLenses = (rows, cols) => diagonals(rows, cols).map(coordLens);
-
-const sortMatrix = (comparator) => (m) =>
-  diagonalLenses(m.length, m[0].length).reduce(
-    (m, lens) => lens.set(lens.get(m).sort(comparator), m),
-    m
-  );
-
-const numericSorter = (a, b) => a - b;
-
-console.log(
-  sortMatrix(numericSorter)([
-    [1, 3, 9, 4],
-    [9, 5, 7, 7],
-    [3, 6, 9, 7],
-    [1, 2, 2, 2],
-  ])
-    .map((r) => r.join(" "))
-    .join("\n")
-);
-*/
+ [
+ [ 1, 3, 3, 1  ],
+ [ 9, 5, 4, 2  ],
+ [ 9, 6, 7, 2  ],
+ [ 7, 9, 7, 2  ]
+ ]
+ */
