750deb3b542c848fa643785b6d7647ee78e5a493
khanr <khanriza@gmail.com>
2021-03-08 21:20:09 -0500

    diagonalsort

--- diagonalsSort.test.js
+++ diagonalsSort.test.js
@@ -1,43 +1,40 @@
 function diagonalSort(m) {
-  const rows = m.length;
-  const cols = m[0].length;
-  const diagonals = rows * cols - 1;
-  const flat = [];
+  const range = (lo, hi) => [...Array(hi - lo + 1)].map((_, i) => i + lo);
+  const diagonals = (rows, cols) =>
+    range(0, rows + cols)
+      .map((d) =>
+        range(0, d)
+          .map((x) => [d - x, x])
+          .filter(([y, x]) => x >= 0 && x < cols && y >= 0 && y < rows)
+      )
+      .filter((xs) => xs.length > 0);
 
-  let newMatrix = Array(rows)
-    .fill(0)
-    .map((x, i) => Array(cols).fill("xxx"));
-
-  for (let k = 0; k <= diagonals; k++) {
-    const temp = [];
-    for (let y = cols - 1; y >= 0; y--) {
-      let x = k - y;
-
-      if (x >= 0 && x < rows) {
-        temp.push(m[y][x]);
-      }
-    }
-
-    if (temp.length) {
-      flat.push(temp.sort());
-    }
-  }
-
-  flat.forEach((o, oI) => {
-    let rowReducer = 0;
-
-    counter = 0;
-    o.forEach((i, iI) => {
-      if (oI > rows) {
-        rowReducer++;
-      }
-      // newMatrix[oI][iI - counter] = i;
-      console.log({ outter: oI - rowReducer }, iI);
-      counter--;
-    });
+  const coordLens = (coords) => ({
+    get: (m) => coords.map(([x, y]) => m[x][y]),
+    set: (arr, m) =>
+      coords.reduce(
+        (m, [x, y], i) => {
+          m[x][y] = arr[i];
+          return m;
+        },
+        m.map((r) => [...r])
+      ),
   });
-}
 
+  const diagonalLenses = (rows, cols) => diagonals(rows, cols).map(coordLens);
+
+  const sortMatrix = (comparator) => (m) =>
+    diagonalLenses(m.length, m[0].length).reduce(
+      (m, lens) => lens.set(lens.get(m).sort(comparator), m),
+      m
+    );
+
+  const numericSorter = (a, b) => a - b;
+
+  console.log(range);
+
+  return sortMatrix(numericSorter)(m).map((r) => r);
+}
 const matrix = [
   [1, 3, 9, 4],
   [9, 5, 7, 7],
@@ -45,20 +42,4 @@ const matrix = [
   [1, 2, 2, 2],
 ];
 
-diagonalSort(matrix);
-
-/*
- [
- [ 1, 3, 3, 1  ],
- [ 9, 5, 4, 2  ],
- [ 9, 6, 7, 2  ],
- [ 7, 9, 7, 2  ]
- ]
-
- [
-   [ {0, 0}, , 3, 1  ],
-   [ {1, 0}, { 1, 1 }, 4, 2  ],
-   [ 9, { 2, 1 }, { 2, 2 }, 2  ],
-   [ { 3, 0 }, { 3, 1 }, 7, 2  ]
- ]
- */
+console.log(diagonalSort(matrix));
